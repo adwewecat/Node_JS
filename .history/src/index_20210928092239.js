c6a3c6d254f564547282bf6ca1f0c4a2c984db6f
adwewecat <89115538+adwewecat@users.noreply.github.com>
2021-09-28 19:25:57 +0700

    Nguyen-Anh-Dung

---
+++ .history/src/index_20210928092239.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+
+app.use(morgan('combined'));
+
+// template Engine 
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
+app.set("view", path.join(__dirname, "views"));
+
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ .history/src/index_20210928095025.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+
+// template Engine
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "resources/views"));
+
+// console.log(path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
